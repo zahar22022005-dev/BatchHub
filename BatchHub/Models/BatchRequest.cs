26ab3f41a17799cbfc7b72c1e2e62e252c9dd080
zahar22022005-dev <zahar22022005@gmail.com>
2026-05-11 22:55:17 +0300

    Delete BatchHub directory

--- BatchHub/Models/BatchRequest.cs
+++
@@ -1,8 +0,0 @@
-namespace BatchHub.Models;
-
-public class BatchRequest
-{
-    public string ProductName { get; set; } = string.Empty;
-    public int Quantity { get; set; }
-    public decimal Price { get; set; }
-}
