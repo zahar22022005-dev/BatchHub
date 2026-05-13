d103b96108d8af9f3ffbb079bce3db2fbe0de4ea
zahar22022005-dev <zahar22022005@gmail.com>
2026-05-13 19:32:23 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -4,7 +4,6 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-// НАСТРОЙКА CORS (разрешаем запросы с любых источников)
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowAll",
@@ -14,17 +13,13 @@ builder.Services.AddCors(options =>
             .AllowAnyHeader());
 });
 
-// Регистрируем HttpClient для вызовов API
-builder.Services.AddHttpClient<PlatformController>();
+builder.Services.AddHttpClient();
 
 var app = builder.Build();
 
 app.UseSwagger();
 app.UseSwaggerUI();
-
-// ИСПОЛЬЗОВАНИЕ CORS (ВАЖНО: ДО HttpsRedirection!)
 app.UseCors("AllowAll");
-
 app.UseHttpsRedirection();
 app.UseAuthorization();
 app.MapControllers();
