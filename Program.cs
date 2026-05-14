a4d65a1fbe109c479121e1bbd16fff6041891e21
zahar22022005-dev <zahar22022005@gmail.com>
2026-05-14 16:35:10 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -3,19 +3,13 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy("AllowAll",
-        policy => policy
-            .AllowAnyOrigin()
-            .AllowAnyMethod()
-            .AllowAnyHeader());
-});
-
+builder.Services.AddCors(...);
 builder.Services.AddHttpClient();
 
 var app = builder.Build();
+
+// ТЕСТОВЫЙ ЭНДПОИНТ
+app.MapGet("/ping", () => "pong");
 
 app.UseSwagger();
 app.UseSwaggerUI();
